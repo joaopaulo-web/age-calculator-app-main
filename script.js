b0b934d695fdfecbed03971d32fe060e7a5ed140
João Paulo Coimbra <joaopaulocoimbra2013@gmail.com>
2023-12-05 10:49:57 -0300

    Alteração no código para que os erros sumam quando o botão for clicado uma segunda vez

--- script.js
+++ script.js
@@ -1,81 +1,89 @@
 function hideRespostas() {
+    document.querySelector('#resposta-form').style.visibility = 'hidden';
+}
 
-    document.querySelector('#resposta-form').style.visibility = 'hidden'
+function resetErrorState() {
+    document.querySelector('.error').style.border = 'none';
+    document.querySelector('#error-msg').innerHTML = '';
 
+    document.querySelector('#mes-entrada').style.border = 'none';
+    document.querySelector('#error-msg-mes').innerHTML = '';
+
+    document.querySelector('#ano-entrada').style.border = 'none';
+    document.querySelector('#error-msg-ano').innerHTML = '';
 }
 
 function sendForm() {
+    // Redefine o estado de erro
+    resetErrorState();
 
-    document.querySelector('#resposta-form').style.visibility = 'visible'
+    document.querySelector('#resposta-form').style.visibility = 'visible';
 
     // Atribui o ano, mes e dia atual
-    const date = new Date()
-
-    const anoAtual = parseFloat(date.getFullYear())
-
-    const mesAtual = date.getMonth() + 1
-
-    const diaAtual = parseFloat(date.getDate())
-
+    const date = new Date();
+    const anoAtual = parseFloat(date.getFullYear());
+    const mesAtual = date.getMonth() + 1;
+    const diaAtual = parseFloat(date.getDate());
 
     // Pega os valores do formulário
-    var valorDia = parseFloat(document.querySelector('#dia-entrada').value)
-
-    var valorMes = parseFloat(document.querySelector('#mes-entrada').value)
-
-    var valorAno = parseFloat(document.querySelector('#ano-entrada').value)
+    var valorDia = parseFloat(document.querySelector('#dia-entrada').value);
+    var valorMes = parseFloat(document.querySelector('#mes-entrada').value);
+    var valorAno = parseFloat(document.querySelector('#ano-entrada').value);
 
     // Amostragem de Anos
-    var idadeAtual = parseFloat(anoAtual - valorAno)
-
-    var displayIdade = document.querySelector('#resultado-anos')
-    displayIdade.innerHTML = idadeAtual
-
+    var idadeAtual = parseFloat(anoAtual - valorAno);
+    var displayIdade = document.querySelector('#resultado-anos');
+    displayIdade.innerHTML = idadeAtual;
 
     // Amostragem de Meses
-
-    var mesesAtual = parseFloat(valorMes - mesAtual)
-
+    var mesesAtual = parseFloat(valorMes - mesAtual);
     if (mesesAtual < 0) {
-        var mesesConvertido = Math.abs(mesesAtual)
-        var displayMeses = document.querySelector('#resultado-meses')
-        displayMeses.innerHTML = mesesConvertido
+        var mesesConvertido = Math.abs(mesesAtual);
+        var displayMeses = document.querySelector('#resultado-meses');
+        displayMeses.innerHTML = mesesConvertido;
     } else {
-        var displayMeses = document.querySelector('#resultado-meses')
-        displayMeses.innerHTML = mesesAtual
+        var displayMeses = document.querySelector('#resultado-meses');
+        displayMeses.innerHTML = mesesAtual;
     }
 
-
     // Amostragem de Dias
-    var diasAtuais = parseFloat(diaAtual - valorDia)
-
+    var diasAtuais = parseFloat(diaAtual - valorDia);
     if (diasAtuais < 0) {
-        var diasConvertido = Math.abs(diasAtuais)
-        var displayDias = document.querySelector('#resultado-dias')
-        displayDias.innerHTML = diasConvertido
+        var diasConvertido = Math.abs(diasAtuais);
+        var displayDias = document.querySelector('#resultado-dias');
+        displayDias.innerHTML = diasConvertido;
     } else {
-        var displayDias = document.querySelector('#resultado-dias')
-        displayDias.innerHTML = diasAtuais
+        var displayDias = document.querySelector('#resultado-dias');
+        displayDias.innerHTML = diasAtuais;
     }
 
     // Erros de inputs vazios
-
     if (document.querySelector('#dia-entrada').value === '') {
-        displayDias.innerHTML = '--'
-        document.querySelector('.error').style.border = 'solid 1px hsl(0, 100%, 67%)'
-        document.querySelector('#error-msg').innerHTML = 'Insira um dia válido'
+        displayDias.innerHTML = '--';
+        document.querySelector('.error').style.border = 'solid 1px hsl(0, 100%, 67%)';
+        document.querySelector('#error-msg').innerHTML = 'Insira um dia válido';
     }
 
     if (document.querySelector('#mes-entrada').value === '') {
-        displayMeses.innerHTML = '--'
-        document.querySelector('#mes-entrada').style.border = 'solid 1px hsl(0, 100%, 67%)'
-        document.querySelector('#error-msg-mes').innerHTML = 'Insira um mês válido'
+        displayMeses.innerHTML = '--';
+        document.querySelector('#mes-entrada').style.border = 'solid 1px hsl(0, 100%, 67%)';
+        document.querySelector('#error-msg-mes').innerHTML = 'Insira um mês válido';
     }
 
     if (document.querySelector('#ano-entrada').value === '') {
-        displayIdade.innerHTML = '--'
-        document.querySelector('#ano-entrada').style.border = 'solid 1px hsl(0, 100%, 67%)'
-        document.querySelector('#error-msg-ano').innerHTML = 'Insira um ano válido'
+        displayIdade.innerHTML = '--';
+        document.querySelector('#ano-entrada').style.border = 'solid 1px hsl(0, 100%, 67%)';
+        document.querySelector('#error-msg-ano').innerHTML = 'Insira um ano válido';
     }
-
 }
+
+// Evento de clique no botão de enviar
+document.querySelector('#seu-botao-enviar').addEventListener('click', function () {
+    // Preencha os campos dos inputs
+    document.querySelector('#dia-entrada').value = 'seu-valor-dia';
+    document.querySelector('#mes-entrada').value = 'seu-valor-mes';
+    document.querySelector('#ano-entrada').value = 'seu-valor-ano';
+
+    // Chame a função para enviar o formulário
+    sendForm();
+});
