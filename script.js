6c4405a141df89c9563a3129cc0a862affb115b8
João Paulo Coimbra <joaopaulocoimbra2013@gmail.com>
2023-12-05 10:42:07 -0300

    Adição das condicionais de error

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function hideRespostas() {
 
 }
 
-function sendForm(){
+function sendForm() {
 
     document.querySelector('#resposta-form').style.visibility = 'visible'
 
@@ -25,7 +25,6 @@ function sendForm(){
 
     var valorAno = parseFloat(document.querySelector('#ano-entrada').value)
 
-
     // Amostragem de Anos
     var idadeAtual = parseFloat(anoAtual - valorAno)
 
@@ -34,16 +33,49 @@ function sendForm(){
 
 
     // Amostragem de Meses
+
     var mesesAtual = parseFloat(valorMes - mesAtual)
-    
-    var displayMeses = document.querySelector('#resultado-meses')
-    displayMeses.innerHTML = mesesAtual
+
+    if (mesesAtual < 0) {
+        var mesesConvertido = Math.abs(mesesAtual)
+        var displayMeses = document.querySelector('#resultado-meses')
+        displayMeses.innerHTML = mesesConvertido
+    } else {
+        var displayMeses = document.querySelector('#resultado-meses')
+        displayMeses.innerHTML = mesesAtual
+    }
 
 
     // Amostragem de Dias
-    var diasAtuais = parseFloat( diaAtual - valorDia)
+    var diasAtuais = parseFloat(diaAtual - valorDia)
 
-    var displayDias = document.querySelector('#resultado-dias')
-    displayDias.innerHTML = diasAtuais
+    if (diasAtuais < 0) {
+        var diasConvertido = Math.abs(diasAtuais)
+        var displayDias = document.querySelector('#resultado-dias')
+        displayDias.innerHTML = diasConvertido
+    } else {
+        var displayDias = document.querySelector('#resultado-dias')
+        displayDias.innerHTML = diasAtuais
+    }
+
+    // Erros de inputs vazios
+
+    if (document.querySelector('#dia-entrada').value === '') {
+        displayDias.innerHTML = '--'
+        document.querySelector('.error').style.border = 'solid 1px hsl(0, 100%, 67%)'
+        document.querySelector('#error-msg').innerHTML = 'Insira um dia válido'
+    }
+
+    if (document.querySelector('#mes-entrada').value === '') {
+        displayMeses.innerHTML = '--'
+        document.querySelector('#mes-entrada').style.border = 'solid 1px hsl(0, 100%, 67%)'
+        document.querySelector('#error-msg-mes').innerHTML = 'Insira um mês válido'
+    }
+
+    if (document.querySelector('#ano-entrada').value === '') {
+        displayIdade.innerHTML = '--'
+        document.querySelector('#ano-entrada').style.border = 'solid 1px hsl(0, 100%, 67%)'
+        document.querySelector('#error-msg-ano').innerHTML = 'Insira um ano válido'
+    }
 
 }
